eb732196fb946a0129e5d03f6922771500ea80c3
Brock Allen <brockallen@gmail.com>
2016-03-25 11:38:50 -0400

    consolidate response parsing into url utility

--- src/UrlUtility.js
+++ src/UrlUtility.js
@@ -1,17 +1,50 @@
+import Log from './Log';
+import Window from './Window';
+
 export default class UrlUtility {
     static addQueryParam(url, name, value) {
-        if (url.indexOf('?') < 0){
+        if (url.indexOf('?') < 0) {
             url += "?";
         }
-        
-        if (url[url.length - 1] !== "?"){
+
+        if (url[url.length - 1] !== "?") {
             url += "&";
         }
-        
+
         url += encodeURIComponent(name);
         url += "=";
         url += encodeURIComponent(value);
-        
+
         return url;
+    }
+
+    static parseUrlFragment(value, window = Window) {
+        Log.info("UrlUtility.parseUrlFragment");
+
+        value = value || window.location.hash;
+
+        var idx = value.lastIndexOf("#");
+        if (idx >= 0) {
+            value = value.substr(idx + 1);
+        }
+
+        var params = {},
+            regex = /([^&=]+)=([^&]*)/g,
+            m;
+
+        var counter = 0;
+        while (m = regex.exec(value)) {
+            params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+            if (counter++ > 50) {
+                Log.error("response exceeded expected number of parameters", value);
+                return {
+                    error: "Response exceeded expected number of parameters"
+                };
+            }
+        }
+
+        for (var prop in params) {
+            return params;
+        }
     }
 }
--- test/UrlUtility.spec.js
+++ test/UrlUtility.spec.js
@@ -11,11 +11,11 @@ describe("UrlUtility", function() {
         it("should add ? if not present", function() {
             UrlUtility.addQueryParam("url", "foo", "test").should.equal("url?foo=test");
         });
-        
+
         it("should not add ? if already present", function() {
             UrlUtility.addQueryParam("url?", "foo", "test").should.equal("url?foo=test");
         });
-        
+
         it("should add & if needed", function() {
             UrlUtility.addQueryParam("url?x=1", "foo", "test").should.equal("url?x=1&foo=test");
         });
@@ -24,5 +24,38 @@ describe("UrlUtility", function() {
             UrlUtility.addQueryParam("url", "#", "#").should.equal("url?%23=%23");
         });
     });
-    
+
+    describe("parseUrlFragment", function() {
+
+        it("should parse key/value pairs", function() {
+            let result = UrlUtility.parseUrlFragment("a=apple&b=banana&c=carrot");
+            result.should.deep.equal({ a: "apple", b: "banana", c: "carrot" });
+        });
+
+        it("should parse any order", function() {
+            let result = UrlUtility.parseUrlFragment("b=banana&c=carrot&a=apple");
+            result.should.deep.equal({ a: "apple", b: "banana", c: "carrot" });
+        });
+
+        it("should parse past host name and hash fragment", function() {
+            let result = UrlUtility.parseUrlFragment("http://server?test1=xoxo&test2=xoxo/#a=apple&b=banana&c=carrot");
+            result.should.deep.equal({ a: "apple", b: "banana", c: "carrot" });
+        });
+
+        it("should return error for long values", function() {
+            let result = UrlUtility.parseUrlFragment("a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple");
+            result.should.have.property('error');
+        });
+
+        it("should use window.location.hash when no value is passed", function() {
+            let w = {
+                location: {
+                    hash: "a=apple&b=banana&c=carrot"
+                }
+            };
+            let result = UrlUtility.parseUrlFragment(null, w);
+            result.should.deep.equal({ a: "apple", b: "banana", c: "carrot" });
+        });
+    });
+
 });
