a99a8a68243489d1cb3c55ac308f52ffaf0feab5
Brock Allen <brockallen@gmail.com>
2016-03-27 12:00:38 -0400

    update from tldrlegal.com

--- src/random.js
+++ src/random.js
@@ -1,6 +1,27 @@
 // NOTICE: the code in this file originally developed by Microsoft
+
+//----------------------------------------------------------------------
+// AdalJS v1.0.8
+// @preserve Copyright (c) Microsoft Open Technologies, Inc.
+// All Rights Reserved
+// Apache License 2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//----------------------------------------------------------------------
+
 // source: https://github.com/AzureAD/azure-activedirectory-library-for-js/blob/master/lib/adal.js#L1029
 // original license: https://github.com/AzureAD/azure-activedirectory-library-for-js/blob/master/LICENSE.txt
+
 export default function random() {
     var guidHolder = 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx';
     var hex = '0123456789abcdef';
