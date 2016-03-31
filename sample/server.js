bb29914465c3126de61ea65229d1a5db71bd0663
Brock Allen <brockallen@gmail.com>
2016-03-31 09:55:12 -0400

    update sample

--- sample/server.js
+++ sample/server.js
@@ -1,14 +1,17 @@
+var open = require('open');
+
 var port = 5000;
 var url = "http://localhost:" + port;
 
 var express = require('express');
 var app = express();
 
-var static = express.static('public');
+var static = express.static(__dirname + '/public');
 app.use(static);
 
 var oidc = require('./oidc.js');
 oidc(url, app);
 
 console.log("listening on " + url);
+open(url);
 app.listen(port);
