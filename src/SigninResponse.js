f231ec9d80f29c7590ca0ccde94bb7c5f61b45fc
Brock Allen <brockallen@gmail.com>
2016-04-03 18:46:18 -0400

    add scopes, expires_in, expired

--- src/SigninResponse.js
+++ src/SigninResponse.js
@@ -4,32 +4,44 @@ import ErrorResponse from './ErrorResponse';
 
 export default class SigninResponse {
     constructor(url) {
-        
+
         var values = UrlUtility.parseUrlFragment(url, "#");
-        
-        if (values.error){
+
+        if (values.error) {
             return new ErrorResponse(values);
         }
-        
+
         this.state = values.state;
         this.id_token = values.id_token;
         this.session_state = values.session_state;
         this.access_token = values.access_token;
         this.token_type = values.token_type;
         this.scope = values.scope;
-        
+
         let expires_in = parseInt(values.expires_in);
-        if (typeof expires_in === 'number' && expires_in > 0){
+        if (typeof expires_in === 'number' && expires_in > 0) {
             let now = parseInt(Date.now() / 1000);
             this.expires_at = now + expires_in;
         }
     }
-    
-    get expires_in(){
-        if (this.expires_at){
+
+    get expires_in() {
+        if (this.expires_at) {
             let now = parseInt(Date.now() / 1000);
             return this.expires_at - now;
         }
         return undefined;
+    }
+
+    get expired() {
+        let expires_in = this.expires_in;
+        if (expires_in !== undefined) {
+            return expires_in <= 0;
+        }
+        return undefined;
+    }
+
+    get scopes() {
+        return (this.scope || "").split(" ");
     }
 }
--- test/unit/SigninResponse.spec.js
+++ test/unit/SigninResponse.spec.js
@@ -14,62 +14,105 @@ describe("SigninResponse", function() {
             let subject = new SigninResponse("state=foo");
             subject.state.should.equal("foo");
         });
-        
+
         it("should return SigninResponse if no error", function() {
             let subject = new SigninResponse("");
             subject.should.be.instanceof(SigninResponse);
         });
-        
+
         it("should return ErrorResponse if error", function() {
             let subject = new SigninResponse("error=foo");
             subject.should.be.instanceof(ErrorResponse);
         });
-        
+
         it("should read id_token", function() {
             let subject = new SigninResponse("id_token=foo");
             subject.id_token.should.equal("foo");
         });
-        
+
         it("should read session_state", function() {
             let subject = new SigninResponse("session_state=foo");
             subject.session_state.should.equal("foo");
         });
-        
+
         it("should read access_token", function() {
             let subject = new SigninResponse("access_token=foo");
             subject.access_token.should.equal("foo");
         });
-        
+
         it("should read token_type", function() {
             let subject = new SigninResponse("token_type=foo");
             subject.token_type.should.equal("foo");
         });
-        
+
         it("should read scope", function() {
             let subject = new SigninResponse("scope=foo");
             subject.scope.should.equal("foo");
         });
-        
+
         it("should read expires_in", function() {
             let subject = new SigninResponse("expires_in=10");
             subject.expires_in.should.equal(10);
         });
-        
+
         it("should calculate expires_at", function() {
             let subject = new SigninResponse("expires_in=10");
-            subject.expires_at.should.equal(parseInt((Date.now()/1000) + 10));
+            subject.expires_at.should.equal(parseInt((Date.now() / 1000) + 10));
         });
-        
+
         it("should not read invalid expires_in", function() {
             let subject = new SigninResponse("expires_in=foo");
             expect(subject.expires_in).to.be.undefined;
             expect(subject.expires_at).to.be.undefined;
-            
+
             subject = new SigninResponse("expires_in=-10");
             expect(subject.expires_in).to.be.undefined;
             expect(subject.expires_at).to.be.undefined;
         });
 
+    });
+
+    describe("scopes", function() {
+        it("should return list of scope", function() {
+            let subject = new SigninResponse("scope=foo");
+            subject.scopes.should.deep.equal(["foo"]);
+
+            subject = new SigninResponse("scope=foo%20bar");
+            subject.scopes.should.deep.equal(["foo", "bar"]);
+            
+             subject = new SigninResponse("scope=foo%20bar%20baz");
+            subject.scopes.should.deep.equal(["foo", "bar", "baz"]);
+        });
+    });
+
+    describe("expires_in", function() {
+        it("should calculate how much time left", function() {
+            Date.now = function() {
+                return 1000 * 1000; // ms
+            }
+            let subject = new SigninResponse("expires_in=100");
+            subject.expires_in.should.equal(100);
+            
+            Date.now = function() {
+                return 1050 * 1000;
+            }
+            subject.expires_in.should.equal(50);
+        });
+    });
+    
+    describe("expired", function() {
+        it("should calculate how much time left", function() {
+            Date.now = function() {
+                return 1000 * 1000; // ms
+            }
+            let subject = new SigninResponse("expires_in=100");
+            subject.expired.should.be.false;
+
+            Date.now = function() {
+                return 1100 * 1000; // ms
+            }
+            subject.expired.should.be.true;
+        });
     });
 
 });
