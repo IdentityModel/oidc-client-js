ad62b752f0d3a506c60159de6f0e6da5efce2e1e
Brock Allen <brockallen@gmail.com>
2016-04-04 13:35:25 -0400

    add get and getAllKeys APIs

--- src/WebStorageStateStore.js
+++ src/WebStorageStateStore.js
@@ -2,34 +2,53 @@ import Log from './Log';
 import Window from './Window';
 
 export default class WebStorageStateStore {
-    constructor({prefix = "oidc.", store = Window.localStorage} = {}){
+    constructor({prefix = "oidc.", store = Window.localStorage} = {}) {
         this._store = store;
         this._prefix = prefix;
     }
-    
-    set(key, value){
+
+    set(key, value) {
         Log.info("WebStorageStateStore.set", key);
-        
-        key = this._prefix + key;
-        
-        try{
-            this._store.setItem(key, value);
-            return Promise.resolve();
-        }
-        catch(e){
-            Log.error("storage exception", e);
-            return Promise.reject(new Error("Failed to store item", key));
-        }
-    }   
-    
-    remove(key){
-        Log.info("WebStorageStateStore.remove", key);
-        
+
         key = this._prefix + key;
 
-        var item = this._store.getItem(key);
+        this._store.setItem(key, value);
+        
+        return Promise.resolve();
+    }
+
+    get(key) {
+        Log.info("WebStorageStateStore.get", key);
+
+        key = this._prefix + key;
+
+        let item = this._store.getItem(key);
+        
+        return Promise.resolve(item);
+    }
+
+    remove(key) {
+        Log.info("WebStorageStateStore.remove", key);
+
+        key = this._prefix + key;
+
+        let item = this._store.getItem(key);
         this._store.removeItem(key);
         
         return Promise.resolve(item);
-    }   
+    }
+
+    getAllKeys() {
+        var keys = [];
+
+        for (let index = 0; index < this._store.length; index++) {
+            let key = this._store.key(index);
+            
+            if (key.indexOf(this._prefix) === 0) {
+                keys.push(key);
+            }
+        }
+        
+        return Promise.resolve(keys);
+    }
 }
--- test/unit/WebStorageStateStore.spec.js
+++ test/unit/WebStorageStateStore.spec.js
@@ -15,7 +15,7 @@ describe("WebStorageStateStore", function() {
     beforeEach(function() {
         prefix = "";
         store = new InMemoryWebStorage();
-        subject = new WebStorageStateStore({prefix:prefix, store:store});
+        subject = new WebStorageStateStore({ prefix: prefix, store: store });
     });
 
     describe("set", function() {
@@ -25,17 +25,17 @@ describe("WebStorageStateStore", function() {
         });
 
         it("should store item", function(done) {
-            subject.set("key", "value").then(()=>{
+            subject.set("key", "value").then(() => {
                 store.getItem("key").should.equal("value");
                 done();
             })
         });
-        
-        it("should use prefix if specified", function(){
-            prefix = "foo.";
-            subject = new WebStorageStateStore({prefix:prefix, store:store});
 
-            subject.set("key", "value").then(()=>{
+        it("should use prefix if specified", function() {
+            prefix = "foo.";
+            subject = new WebStorageStateStore({ prefix: prefix, store: store });
+
+            subject.set("key", "value").then(() => {
                 store.getItem(prefix + "key").should.equal("value");
                 done();
             })
@@ -74,14 +74,75 @@ describe("WebStorageStateStore", function() {
             });
         });
 
-        it("should use prefix if specified", function(done){
+        it("should use prefix if specified", function(done) {
             prefix = "foo.";
-            subject = new WebStorageStateStore({prefix:prefix, store:store});
+            subject = new WebStorageStateStore({ prefix: prefix, store: store });
 
             store.setItem("foo.key", "value");
 
             subject.remove("key").then(item => {
                 expect(store.getItem("foo.key")).to.be.undefined;
+                done();
+            });
+        });
+
+    });
+
+    describe("get", function() {
+
+        it("should return a promise", function() {
+            subject.get("key").should.be.instanceof(Promise);
+        });
+
+        it("should return value if exists", function(done) {
+            store.setItem("key", "test");
+
+            subject.get("key").then(value => {
+                value.should.equal('test');
+                done();
+            });
+        });
+
+        it("should return undefined if doesn't exist", function(done) {
+            subject.get("key").then(value => {
+                expect(value).to.be.undefined;
+                done();
+            });
+        });
+
+        it("should use prefix if specified", function(done) {
+            prefix = "foo.";
+            subject = new WebStorageStateStore({ prefix: prefix, store: store });
+
+            store.setItem("foo.key", "value");
+
+            subject.get("key").then(item => {
+                item.should.equal("value");
+                done();
+            });
+        });
+
+    });
+
+    describe("getAllKeys", function() {
+
+        it("should return a promise", function() {
+            subject.getAllKeys().should.be.instanceof(Promise);
+        });
+
+        it("should return keys", function(done) {
+            store.setItem("key1", "test");
+            store.setItem("key2", "test");
+
+            subject.getAllKeys().then(keys => {
+                keys.should.deep.equal(["key1", "key2"]);
+                done();
+            });
+        });
+        
+        it("should return empty keys when empty", function(done) {
+            subject.getAllKeys().then(keys => {
+                keys.should.deep.equal([]);
                 done();
             });
         });
