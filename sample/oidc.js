93f83f7db6449c840f9650e4b685c5818d1605df
Brock Allen <brockallen@gmail.com>
2016-04-06 20:07:37 -0400

    formatting

--- sample/oidc.js
+++ sample/oidc.js
@@ -21,9 +21,9 @@ var metadata = {
     end_session_endpoint: endSessionPath,
 };
 
-function prependBaseUrlToMetadata(baseUrl){
-    for(var name in metadata){
-        metadata[name] = baseUrl + metadata[name]; 
+function prependBaseUrlToMetadata(baseUrl) {
+    for (var name in metadata) {
+        metadata[name] = baseUrl + metadata[name];
     }
 }
 
@@ -40,17 +40,17 @@ var keys = {
 };
 
 var claims = {
-    "sub":"818727",
-    "email":"AliceSmith@email.com",
-    "email_verified":true,
-    "role":["Admin","Geek"]
+    "sub": "818727",
+    "email": "AliceSmith@email.com",
+    "email_verified": true,
+    "role": ["Admin", "Geek"]
 };
 
-function genAccessToken(){
+function genAccessToken() {
     return parseInt(Math.random().toString().replace("0.", "")).toString(16);
 }
 
-function hashAccessToken(access_token){
+function hashAccessToken(access_token) {
     var hash = jsrsasign.crypto.Util.hashString(access_token, "sha256")
     var left = hash.substr(0, hash.length / 2);
     var left_b64u = jsrsasign.hextob64u(left);
@@ -58,17 +58,17 @@ function hashAccessToken(access_token){
 }
 
 function genIdToken(aud, nonce, access_token) {
-    var now = parseInt(Date.now()/1000);
+    var now = parseInt(Date.now() / 1000);
     var payload = {
-        aud : aud,
-        iss : metadata.issuer,
-        nonce : nonce,
-        sid : "37889234079034890",
-        nbf : now,
-        iat : now,
-        exp : now + 300,
-        idp : "some_idp", 
-        amr : [ "password" ]
+        aud: aud,
+        iss: metadata.issuer,
+        nonce: nonce,
+        sid: "37889234079034890",
+        nbf: now,
+        iat: now,
+        exp: now + 300,
+        idp: "some_idp",
+        amr: ["password"]
     };
 
     if (access_token) {
@@ -76,12 +76,12 @@ function genIdToken(aud, nonce, access_token) {
         payload.sub = claims.sub;
     }
     else {
-        for(var key in claims){
+        for (var key in claims) {
             payload[key] = claims[key];
-        }    
+        }
     }
-    
-    return jsrsasign.jws.JWS.sign(null, {alg: "RS256", kid: "1"}, payload, rsaKey.prvKeyObj);
+
+    return jsrsasign.jws.JWS.sign(null, { alg: "RS256", kid: "1" }, payload, rsaKey.prvKeyObj);
 }
 
 function isOidc(response_type) {
@@ -113,56 +113,56 @@ function addFragment(url, name, value) {
 
     return url;
 }
-    
+
 module.exports = function(baseUrl, app) {
     prependBaseUrlToMetadata(baseUrl);
-    
-    app.get(metadataPath, function (req, res) {
+
+    app.get(metadataPath, function(req, res) {
         res.json(metadata);
     });
-    
-    app.get(signingKeysPath, function (req, res) {
+
+    app.get(signingKeysPath, function(req, res) {
         res.json(keys);
     });
-    
-    app.get(authorizationPath, function (req, res) {
+
+    app.get(authorizationPath, function(req, res) {
         var response_type = req.query.response_type;
-         
+
         var url = req.query.redirect_uri;
-        
-        if (isOAuth(response_type)){
+
+        if (isOAuth(response_type)) {
             var access_token = genAccessToken();
             url = addFragment(url, "access_token", access_token);
             url = addFragment(url, "token_type", "Bearer");
             url = addFragment(url, "expires_in", "3600");
             url = addFragment(url, "scope", req.query.scope);
         }
-        
-        if (isOidc(response_type)){
+
+        if (isOidc(response_type)) {
             url = addFragment(url, "id_token", genIdToken(req.query.client_id, req.query.nonce, access_token));
             url = addFragment(url, "session_state", "123");
         }
-        
+
         var state = req.query.state;
-        if (state){
+        if (state) {
             url = addFragment(url, "state", state);
         }
-        
+
         res.redirect(url);
     });
-    
-    app.get(userInfoPath, function (req, res) {
+
+    app.get(userInfoPath, function(req, res) {
         res.json(claims);
     });
-    
-    app.get(endSessionPath, function (req, res) {
+
+    app.get(endSessionPath, function(req, res) {
         var url = req.query.post_logout_redirect_uri;
-        if (url){
+        if (url) {
             var state = req.query.state;
             res.redirect(url + "?state=" + state);;
         }
         else {
             res.send("logged out");
         }
-    });    
+    });
 }
