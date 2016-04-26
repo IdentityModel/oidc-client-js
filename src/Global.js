6ddaa44120bab885621a82e05a875b00d42211b6
Brock Allen <brockallen@gmail.com>
2016-04-26 08:52:20 -0400

    formatting

--- src/Global.js
+++ src/Global.js
@@ -2,16 +2,16 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 const timer = {
-    setTimeout : function (cb, duration) {
+    setTimeout: function (cb, duration) {
         return setTimeout(cb, duration);
     },
-    clearTimeout : function(handle){
+    clearTimeout: function (handle) {
         return clearTimeout(handle);
     }
 };
 
 export default class Global {
-    static get location(){
+    static get location() {
         return location;
     }
     static get localStorage() {
@@ -20,10 +20,10 @@ export default class Global {
     static get sessionStorage() {
         return sessionStorage;
     }
-    static get XMLHttpRequest(){
+    static get XMLHttpRequest() {
         return XMLHttpRequest;
     }
-    static get timer(){
+    static get timer() {
         return timer;
     }
 };
