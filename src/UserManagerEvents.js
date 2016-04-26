b6d529133ea0f0c21c1d9772a439b0f9aa6ec446
Brock Allen <brockallen@gmail.com>
2016-04-25 23:39:40 -0400

    provide user manager events abstraction

---
+++ src/UserManagerEvents.js
@@ -0,0 +1,20 @@
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+import Log from './Log';
+import AccessTokenEvents from './AccessTokenEvents';
+
+export default class UserManagerEvents extends AccessTokenEvents {
+
+    constructor(settings) {
+        super(settings);
+    }
+
+    init(user) {
+        super.init(user);
+    }
+
+    cancel() {
+        super.cancel();
+    }
+}
