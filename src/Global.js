003910dd5e16ecb8216eba1278eb7060937f2c3c
Brock Allen <brockallen@gmail.com>
2016-04-25 23:38:25 -0400

    ensure timer API is bound to global scope (window)

--- src/Global.js
+++ src/Global.js
@@ -1,6 +1,15 @@
 // Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
+const timer = {
+    setTimeout : function (cb, duration) {
+        return setTimeout(cb, duration);
+    },
+    clearTimeout : function(handle){
+        return clearTimeout(handle);
+    }
+};
+
 export default class Global {
     static get location(){
         return location;
@@ -15,8 +24,6 @@ export default class Global {
         return XMLHttpRequest;
     }
     static get timer(){
-        return {
-            setTimeout, clearTimeout
-        };
+        return timer;
     }
 };
