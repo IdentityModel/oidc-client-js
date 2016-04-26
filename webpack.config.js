6acef20eb76e034c6c43c0e25bb0d752cecab4fd
Brock Allen <brockallen@gmail.com>
2016-04-26 08:59:25 -0400

    add minify plugin

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,14 @@ module.exports = {
         libraryTarget: 'var',
         library: 'IdentityModel'
     },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                screw_ie8: true,
+            },
+        })
+    ],
     node: {
         fs: 'empty', // Because of jsrsasign usage of fs
     },
