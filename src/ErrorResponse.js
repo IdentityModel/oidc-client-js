b4a85d04e873e364ae5299229151342d19552470
Brock Allen <brockallen@gmail.com>
2016-04-29 19:15:58 -0400

    derive from Error

--- src/ErrorResponse.js
+++ src/ErrorResponse.js
@@ -2,9 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 import Log from './Log';
-import UrlUtility from './UrlUtility';
 
-export default class ErrorResponse {
+export default class ErrorResponse extends Error {
     constructor({error, error_description, error_uri, state}={}
     ) {
          if (!error){
@@ -12,9 +11,9 @@ export default class ErrorResponse {
             throw new Error("error");
         }
         
-        this.message = error_description || error;
+        super(error_description || error);
+        
         this.name = "ErrorResponse"; 
-        this.stack = (new Error()).stack;
         
         this.error = error;
         this.error_description = error_description;
