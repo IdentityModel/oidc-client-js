be337d5a81a2e8c5d5f4f6e336342861c2d85596
Maximilian Mantz <maximilian.mantz@yahoo.de>
2016-05-24 21:23:37 +0200

    Added the entry option to the config creator function

--- webpack.base.js
+++ webpack.base.js
@@ -1,6 +1,7 @@
 // create a webpack configuration with all common parts included here
 var createWebpackConfig = function(options) {
   return {
+    entry: options.entry,
     output: options.output,
     plugins: options.plugins,
     node: {
