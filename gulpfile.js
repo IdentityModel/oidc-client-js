4af5b3046b575e736253863c117bd91474ad4a7c
anwalkers <andy.walkers@gmail.com>
2016-07-01 16:37:57 -0700

    compiled from typescript

--- gulpfile.js
+++ gulpfile.js
@@ -8,75 +8,87 @@ var npmEntry = './index.ts';
 var classicEntry = [npmEntry];
 
 // uglify plugin for minification
-var uglifyPlugins =  [
+var uglifyPlugins = [
   new webpack.optimize.UglifyJsPlugin({
-      compress: {
-          warnings: false,
-          screw_ie8: true,
-      },
+    compress: {
+      warnings: false,
+      screw_ie8: true,
+    },
   })
 ];
 
 // npm compliant build with source-maps
-gulp.task('build-lib-sourcemap', function() {
+gulp.task('build-lib-sourcemap', function () {
   // run webpack
   gulp.src('./index.ts').pipe(webpackStream(createWebpackConfig({
     entry: npmEntry,
     output: {
-        filename: 'oidc-client.js',
-        libraryTarget: 'umd'
+      filename: 'oidc-client.js',
+      libraryTarget: 'umd'
     },
     plugins: [],
     devtool: 'inline-source-map'
   })))
-  .pipe(gulp.dest('lib/'));
+    .on('error', function handleError() {
+      this.emit('end'); // Recover from errors
+    })
+    .pipe(gulp.dest('lib/'));
 });
 
 // npm compliant build without source-maps & minified
-gulp.task('build-lib-min', function() {
+gulp.task('build-lib-min', function () {
   // run webpack
   gulp.src('index.ts').pipe(webpackStream(createWebpackConfig({
     entry: npmEntry,
     output: {
-        filename: 'oidc-client.min.js',
-        libraryTarget: 'umd',
+      filename: 'oidc-client.min.js',
+      libraryTarget: 'umd',
     },
     plugins: uglifyPlugins,
     devtool: null
   })))
-  .pipe(gulp.dest('lib/'));
+    .on('error', function handleError() {
+      this.emit('end'); // Recover from errors
+    })
+    .pipe(gulp.dest('lib/'));
 });
 
 // classic build with sourcemaps
-gulp.task('build-dist-sourcemap', function() {
+gulp.task('build-dist-sourcemap', function () {
   // run webpack
   gulp.src('index.ts').pipe(webpackStream(createWebpackConfig({
     entry: classicEntry,
     output: {
-        filename: 'oidc-client.js',
-        libraryTarget: 'var',
-        library: 'Oidc'
+      filename: 'oidc-client.js',
+      libraryTarget: 'var',
+      library: 'Oidc'
     },
     plugins: [],
     devtool: 'inline-source-map'
   })))
-  .pipe(gulp.dest('dist/'));
+    .on('error', function handleError() {
+      this.emit('end'); // Recover from errors
+    })
+    .pipe(gulp.dest('dist/'));
 });
 
 // classic build without sourcemaps & minified
-gulp.task('build-dist-min', function() {
+gulp.task('build-dist-min', function () {
   // run webpack
   gulp.src('index.ts').pipe(webpackStream(createWebpackConfig({
     entry: classicEntry,
     output: {
-        filename: 'oidc-client.min.js',
-        libraryTarget: 'var',
-        library: 'Oidc'
+      filename: 'oidc-client.min.js',
+      libraryTarget: 'var',
+      library: 'Oidc'
     },
     plugins: uglifyPlugins,
     devtool: null
   })))
-  .pipe(gulp.dest('dist/'));
+    .on('error', function handleError() {
+      this.emit('end'); // Recover from errors
+    })
+    .pipe(gulp.dest('dist/'));
 });
 
 // putting it all together
