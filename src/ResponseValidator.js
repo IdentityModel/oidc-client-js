b7ba3d34cc6ba79c7f74056bed15276966b5689c
Manu Rosa <manurosa11@gmail.com>
2017-04-04 18:17:06 +0200

    Fix log and error messages
    The messsage was referring to id_token but it means access_token which is
    used by the userinfo endpoint

--- src/ResponseValidator.js
+++ src/ResponseValidator.js
@@ -136,8 +136,8 @@ export default class ResponseValidator {
                     Log.debug("user info claims received from user info endpoint");
 
                     if (claims.sub !== response.profile.sub) {
-                        Log.error("sub from user info endpoint does not match sub in id_token");
-                        return Promise.reject(new Error("sub from user info endpoint does not match sub in id_token"));
+                        Log.error("sub from user info endpoint does not match sub in access_token");
+                        return Promise.reject(new Error("sub from user info endpoint does not match sub in access_token"));
                     }
 
                     response.profile = this._mergeClaims(response.profile, claims);
