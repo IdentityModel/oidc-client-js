026a8a151054998d96ee73eb0473f3e530fcd3ef
Rafael Staib <me@rafaelstaib.com>
2018-01-23 09:40:01 +0100

    Readded extraQueryParams and added readonly modifier for completion

--- index.d.ts
+++ index.d.ts
@@ -115,27 +115,28 @@ export class OidcClient {
 
 export interface OidcClientSettings {
   authority?: string;
-  metadataUrl?: string;
+  readonly metadataUrl?: string;
   metadata?: any;
   signingKeys?: any[];
   client_id?: string;
-  response_type?: string;
-  scope?: string;
-  redirect_uri?: string;
-  post_logout_redirect_uri?: string;
-  popup_post_logout_redirect_uri?: string;
-  prompt?: string;
-  display?: string;
-  max_age?: number;
-  ui_locales?: string;
-  acr_values?: string;
-  filterProtocolClaims?: boolean;
-  loadUserInfo?: boolean;
-  staleStateAge?: number;
-  clockSkew?: number;
-  stateStore?: StateStore;
+  readonly response_type?: string;
+  readonly scope?: string;
+  readonly redirect_uri?: string;
+  readonly post_logout_redirect_uri?: string;
+  readonly popup_post_logout_redirect_uri?: string;
+  readonly prompt?: string;
+  readonly display?: string;
+  readonly max_age?: number;
+  readonly ui_locales?: string;
+  readonly acr_values?: string;
+  readonly filterProtocolClaims?: boolean;
+  readonly loadUserInfo?: boolean;
+  readonly staleStateAge?: number;
+  readonly clockSkew?: number;
+  readonly stateStore?: StateStore;
   ResponseValidatorCtor?: ResponseValidatorCtor;
   MetadataServiceCtor?: MetadataServiceCtor;
+  extraQueryParams?: {};
 }
 
 export class UserManager extends OidcClient {
@@ -193,21 +194,21 @@ export interface UserManagerEvents extends AccessTokenEvents {
 }
 
 export interface UserManagerSettings extends OidcClientSettings {
-  popup_redirect_uri?: string;
-  popupWindowFeatures?: string;
-  popupWindowTarget?: any;
-  silent_redirect_uri?: any;
-  silentRequestTimeout?: any;
-  automaticSilentRenew?: boolean;
-  includeIdTokenInSilentRenew?: boolean;
-  monitorSession?: boolean;
-  checkSessionInterval?: number;
-  revokeAccessTokenOnSignout?: any;
-  accessTokenExpiringNotificationTime?: number;
-  redirectNavigator?: any;
-  popupNavigator?: any;
-  iframeNavigator?: any;
-  userStore?: any;
+  readonly popup_redirect_uri?: string;
+  readonly popupWindowFeatures?: string;
+  readonly popupWindowTarget?: any;
+  readonly silent_redirect_uri?: any;
+  readonly silentRequestTimeout?: any;
+  readonly automaticSilentRenew?: boolean;
+  readonly includeIdTokenInSilentRenew?: boolean;
+  readonly monitorSession?: boolean;
+  readonly checkSessionInterval?: number;
+  readonly revokeAccessTokenOnSignout?: any;
+  readonly accessTokenExpiringNotificationTime?: number;
+  readonly redirectNavigator?: any;
+  readonly popupNavigator?: any;
+  readonly iframeNavigator?: any;
+  readonly userStore?: any;
 }
 
 export interface WebStorageStateStoreSettings {
