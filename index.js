bcc98ff656757453fd78dd8e74107df105df9aae
Daniel Schaffer <dan@dandoes.net>
2018-03-20 09:52:13 -0700

    fix: export modules from index directly instead of requiring and then re-exporting

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
 // Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-export const Log = require('./src/Log').Log;
-export const OidcClient = require('./src/OidcClient').OidcClient;
-export const OidcClientSettings = require('./src/OidcClientSettings').OidcClientSettings;
-export const WebStorageStateStore = require('./src/WebStorageStateStore').WebStorageStateStore;
-export const InMemoryWebStorage = require('./src/InMemoryWebStorage').InMemoryWebStorage;
-export const UserManager = require('./src/UserManager').UserManager;
-export const AccessTokenEvents = require('./src/AccessTokenEvents').AccessTokenEvents;
-export const MetadataService = require('./src/MetadataService').MetadataService;
-export const CordovaPopupNavigator = require('./src/CordovaPopupNavigator').CordovaPopupNavigator;
-export const CordovaIFrameNavigator = require('./src/CordovaIFrameNavigator').CordovaIFrameNavigator;
-export const CheckSessionIFrame = require('./src/CheckSessionIFrame').CheckSessionIFrame;
-export const TokenRevocationClient = require('./src/TokenRevocationClient').TokenRevocationClient;
-export const SessionMonitor = require('./src/SessionMonitor').SessionMonitor;
-export const Global = require('./src/Global').Global;
-export const User = require('./src/User').User;
-
-export default {
-    Log,
-    OidcClient,
-    OidcClientSettings,
-    WebStorageStateStore,
-    InMemoryWebStorage,
-    UserManager,
-    AccessTokenEvents,
-    MetadataService,
-    CordovaPopupNavigator,
-    CordovaIFrameNavigator,
-    CheckSessionIFrame,
-    TokenRevocationClient,
-    SessionMonitor,
-    Global,
-    User
-};
+export { Log } from './src/Log';
+export { OidcClient } from './src/OidcClient';
+export { OidcClientSettings } from './src/OidcClientSettings';
+export { WebStorageStateStore } from './src/WebStorageStateStore';
+export { InMemoryWebStorage } from './src/InMemoryWebStorage';
+export { UserManager } from './src/UserManager';
+export { AccessTokenEvents } from './src/AccessTokenEvents';
+export { MetadataService } from './src/MetadataService';
+export { CordovaPopupNavigator } from './src/CordovaPopupNavigator';
+export { CordovaIFrameNavigator } from './src/CordovaIFrameNavigator';
+export { CheckSessionIFrame } from './src/CheckSessionIFrame';
+export { TokenRevocationClient } from './src/TokenRevocationClient';
+export { SessionMonitor } from './src/SessionMonitor';
+export { Global } from './src/Global';
+export { User } from './src/User';
