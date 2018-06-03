25d9063f9c3d722d0ae5e49c61ca8eb33d37ee8a
Donal Fenwick <37119470+donalfenwick@users.noreply.github.com>
2018-06-03 18:24:33 +0100

    converting metadataService getter to a property as getters cannot be used in typescript definitions

--- index.d.ts
+++ index.d.ts
@@ -112,7 +112,7 @@ export class OidcClient {
 
   clearStaleState(stateStore: StateStore): Promise<any>;
 
-  get metadataService(): MetadataService;
+  metadataService: MetadataService;
 }
 
 export interface OidcClientSettings {
