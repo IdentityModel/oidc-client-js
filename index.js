7a624c211a9d6a19291be01301dcc9fde39c99c3
Brock Allen <brockallen@gmail.com>
2018-07-05 10:34:07 -0400

    export using default

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 // Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-export { Log } from './src/Log';
-export { OidcClient } from './src/OidcClient';
-export { OidcClientSettings } from './src/OidcClientSettings';
-export { WebStorageStateStore } from './src/WebStorageStateStore';
-export { InMemoryWebStorage } from './src/InMemoryWebStorage';
-export { UserManager } from './src/UserManager';
-export { AccessTokenEvents } from './src/AccessTokenEvents';
-export { MetadataService } from './src/MetadataService';
-export { CordovaPopupNavigator } from './src/CordovaPopupNavigator';
-export { CordovaIFrameNavigator } from './src/CordovaIFrameNavigator';
-export { CheckSessionIFrame } from './src/CheckSessionIFrame';
-export { TokenRevocationClient } from './src/TokenRevocationClient';
-export { SessionMonitor } from './src/SessionMonitor';
-export { Global } from './src/Global';
-export { User } from './src/User';
+import { Log } from './src/Log';
+import { OidcClient } from './src/OidcClient';
+import { OidcClientSettings } from './src/OidcClientSettings';
+import { WebStorageStateStore } from './src/WebStorageStateStore';
+import { InMemoryWebStorage } from './src/InMemoryWebStorage';
+import { UserManager } from './src/UserManager';
+import { AccessTokenEvents } from './src/AccessTokenEvents';
+import { MetadataService } from './src/MetadataService';
+import { CordovaPopupNavigator } from './src/CordovaPopupNavigator';
+import { CordovaIFrameNavigator } from './src/CordovaIFrameNavigator';
+import { CheckSessionIFrame } from './src/CheckSessionIFrame';
+import { TokenRevocationClient } from './src/TokenRevocationClient';
+import { SessionMonitor } from './src/SessionMonitor';
+import { Global } from './src/Global';
+import { User } from './src/User';
+
+export default {
+    Log,
+    OidcClient,
+    OidcClientSettings,
+    WebStorageStateStore,
+    InMemoryWebStorage,
+    UserManager,
+    AccessTokenEvents,
+    MetadataService,
+    CordovaPopupNavigator,
+    CordovaIFrameNavigator,
+    CheckSessionIFrame,
+    TokenRevocationClient,
+    SessionMonitor,
+    Global,
+    User
+};
