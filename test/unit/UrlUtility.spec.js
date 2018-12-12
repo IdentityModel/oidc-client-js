626a0d75cc3a3406cb00d28a2e3043fb40447019
Brock Allen <brockallen@gmail.com>
2018-12-12 13:19:11 -0500

    ignore hash fragment when parsing query string response URLs

--- test/unit/UrlUtility.spec.js
+++ test/unit/UrlUtility.spec.js
@@ -45,6 +45,16 @@ describe("UrlUtility", function() {
             result.should.deep.equal({ a: "apple", b: "banana", c: "carrot" });
         });
 
+        it("should parse query string", function() {
+            let result = UrlUtility.parseUrlFragment("http://server?test1=xoxo&test2=yoyo", "?");
+            result.should.deep.equal({ test1: "xoxo", test2: "yoyo" });
+        });
+        
+        it("should parse query string up to hash", function() {
+            let result = UrlUtility.parseUrlFragment("http://server?test1=xoxo&test2=yoyo#a=apple&b=banana&c=carrot", "?");
+            result.should.deep.equal({ test1: "xoxo", test2: "yoyo" });
+        });
+
         it("should return error for long values", function() {
             let result = UrlUtility.parseUrlFragment("a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple&a=apple");
             result.should.have.property('error');
