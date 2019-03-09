8335caac4f773fb40ca8adbc546b1430aa4f299e
Brock Allen <brockallen@gmail.com>
2019-03-09 15:52:23 -0500

    remove dashes from guid for random API

--- test/unit/random.spec.js
+++ test/unit/random.spec.js
@@ -1,11 +1,13 @@
 import random from '../../src/random'
 
-const pattern = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/
+const pattern = /^[0-9a-f]{8}[0-9a-f]{4}4[0-9a-f]{3}[89ab][0-9a-f]{3}[0-9a-f]{12}$/
+//const pattern = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/
 
 
 describe('random', function() {
-  it('should return a valid RFC4122 v4 guid', function(){
+  it('should return a valid RFC4122 v4 guid (sans dashes)', function(){
     const rnd = random()
+    console.log(rnd);
     rnd.should.match(pattern)
   }) 
 })
