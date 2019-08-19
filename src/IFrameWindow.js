8510cd4df2ba7c29a70212aa182eb056cd38564e
Alexander Hansen <alexanderbh@gmail.com>
2019-08-19 20:00:00 +0200

    Remove the check for "inside a frame"

--- src/IFrameWindow.js
+++ src/IFrameWindow.js
@@ -105,12 +105,10 @@ export class IFrameWindow {
 
     static notifyParent(url) {
         Log.debug("IFrameWindow.notifyParent");
-        if (window.frameElement) {
-            url = url || window.location.href;
-            if (url) {
-                Log.debug("IFrameWindow.notifyParent: posting url message to parent");
-                window.parent.postMessage(url, location.protocol + "//" + location.host);
-            }
+        url = url || window.location.href;
+        if (url) {
+            Log.debug("IFrameWindow.notifyParent: posting url message to parent");
+            window.parent.postMessage(url, location.protocol + "//" + location.host);
         }
     }
 }
