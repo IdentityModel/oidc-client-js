319ac38aa8bfe311418f2e8f0b9844092d5c4bfc
Paul Mowat <paul.mowat@oneadvanced.com>
2020-10-13 13:51:41 +0100

    fix: updating with changes

--- src/IFrameWindow.js
+++ src/IFrameWindow.js
@@ -87,7 +87,7 @@ export class IFrameWindow {
         if (this._timer &&
             e.origin === this._origin &&
             e.source === this._frame.contentWindow &&
-            (e.data.startsWith('http://') || e.data.startsWith('https://'))
+            (typeof e.data === 'string' && (e.data.startsWith('http://') || e.data.startsWith('https://')))
         ) {
             let url = e.data;
             if (url) {
