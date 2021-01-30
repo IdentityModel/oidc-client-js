9e28f3b2ef926a90cefad3c6a6ae195a8f569e83
Brock Allen <brockallen@gmail.com>
2021-01-29 19:13:46 -0500

    1.11.1 build

--- dist/oidc-client.d.ts
+++ dist/oidc-client.d.ts
@@ -166,8 +166,6 @@ export interface OidcClientSettings {
   MetadataServiceCtor?: MetadataServiceCtor;
   /** An object containing additional query string parameters to be including in the authorization request */
   extraQueryParams?: Record<string, any>;
-
-  getEpochTime(): Promise<number>;
 }
 
 export class UserManager extends OidcClient {
